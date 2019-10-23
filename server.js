56cb479dce972b44455131eafed8786d43e7d3dc
KY64 <taurus99@protonmail.com>
2019-10-23 16:43:04 +0700

    Hotfix

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const express   = require('express'),
       fs        = require('fs'),
       app       = express(),
       port      = 3000
+var log
 
 app.use(express.urlencoded({extended: true}))
 app.get('/', (req,res) => res.sendFile(__dirname + '/index.html'))
-app.post('/data', (req,res) => {
-    let fan  = req.body.fan,
-        temp = req.body.temp,
-        time = new Date(),
-        Time = time.toLocaleString()
+app.route('/data')
+   .post((req,res) => {
+        let fan  = req.body.fan,
+            temp = req.body.temp,
+            time = new Date(),
+            Time = time.toLocaleString()
 
-    res.send(req.body)
-    
-    let data = `Fan: ${fan} || temperature: ${temp} || time: ${Time}`
-    
-    fs.appendFile('log.txt', data+'\n', 'utf8', (err) => {
-        if (err) throw err
+        log = req.body
+        res.send(log)
+        
+        let data = `Fan: ${fan} || temperature: ${temp} || time: ${Time}`
+        
+        fs.appendFile('log.txt', data+'\n', 'utf8', (err) => {
+            if (err) throw err
+        })
     })
-})
+    .get((req,res) => res.send(log))
 
 app.listen(port, () => console.log("Listening to port " + port))
