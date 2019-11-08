e005fb2b64ceda5282cbaa22c77a13f22affbc89
KY64 <taurus99@protonmail.com>
2019-11-08 16:15:32 +0700

    Update web page

--- server.js
+++ server.js
@@ -2,26 +2,45 @@ const express   = require('express'),
       fs        = require('fs'),
       app       = express(),
       port      = 3000
-var log
+var data1, data2
 
+app.use(express.static(__dirname + '/'))
 app.use(express.urlencoded({extended: true}))
 app.get('/', (req,res) => res.sendFile(__dirname + '/index.html'))
-app.route('/data')
+app.route('/data/1')
    .post((req,res) => {
         let fan  = req.body.fan,
             temp = req.body.temp,
             time = new Date(),
             Time = time.toLocaleString()
 
-        log = req.body
-        res.send(log)
+        data1 = req.body
+        res.send(data1)
         
         let data = `Fan: ${fan} || temperature: ${temp} || time: ${Time}`
-        
-        fs.appendFile('log.txt', data+'\n', 'utf8', (err) => {
+        fs.appendFile('log1.txt', data+'\n', 'utf8', (err) => {
             if (err) throw err
         })
+
     })
-    .get((req,res) => res.send(log))
+    .get((req,res) => res.send(data1))
+
+app.route('/data/2')
+   .post((req,res) => {
+        let fan  = req.body.fan,
+            temp = req.body.temp,
+            time = new Date(),
+            Time = time.toLocaleString()
+
+        data2 = req.body
+        res.send(data2)
+        
+        let data = `Fan: ${fan} || temperature: ${temp} || time: ${Time}`
+        fs.appendFile('log2.txt', data+'\n', 'utf8', (err) => {
+            if (err) throw err
+        })
+        
+    })
+    .get((req,res) => res.send(data2))
 
 app.listen(port, () => console.log("Listening to port " + port))
